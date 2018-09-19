a9bf71127dc8f135d8e17bb14ddede88e774b6c6
forsrc <forsrc@gmail.com>
2018-09-19 17:46:58 +0800

    zk

--- bigdata/tools/start-zookeeper.sh
+++ bigdata/tools/start-zookeeper.sh
@@ -6,7 +6,7 @@ echo `hostname` ... START
 echo $ZOOKEEPER_HOME/bin/zkServer.sh start
 $ZOOKEEPER_HOME/bin/zkServer.sh start
 
-sleep 3
+sleep 5
 $ZOOKEEPER_HOME/bin/zkServer.sh status
 
 echo `hostname` ... END
