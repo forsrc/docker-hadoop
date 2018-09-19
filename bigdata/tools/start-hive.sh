743ae1440b7ff5e682a88679030cac62d2ce4831
forsrc <forsrc@gmail.com>
2018-09-19 17:24:08 +0800

    hive

--- bigdata/tools/start-hive.sh
+++ bigdata/tools/start-hive.sh
@@ -3,7 +3,13 @@ echo `hostname` ... START
 
 . /root/.bashrc
 
+sleep 60
+
 echo $HIVE_HOME/bin/hive --service metastore
-$HIVE_HOME/bin/hive --service metastore
+nohup $HIVE_HOME/bin/hive --service metastore >/dev/null 2>&1 &
+
+echo $HIVE_HOME/bin/hive --service hiveserver2
+#http://127.0.0.1:10002/
+nohup $HIVE_HOME/bin/hive --service hiveserver2 >/dev/null 2>&1 &
 
 echo `hostname` ... END
