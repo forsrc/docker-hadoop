2bc37ed0aefda1c6555a2dbb2430aa72e5c03c6b
forsrc <forsrc@gmail.com>
2018-10-08 11:48:29 +0800

    hadoop

--- bigdata/tools/start-hadoop.sh
+++ bigdata/tools/start-hadoop.sh
@@ -7,7 +7,8 @@ echo $HADOOP_HOME/sbin/start-dfs.sh
 $HADOOP_HOME/sbin/start-dfs.sh
 echo $HADOOP_HOME/sbin/start-yarn.sh
 $HADOOP_HOME/sbin/start-yarn.sh
-
+echo $HADOOP_HOME/sbin/mr-jobhistory-daemon.sh start historyserver
+$HADOOP_HOME/sbin/mr-jobhistory-daemon.sh start historyserver
 sleep 2
 
 echo `hostname` ... END
