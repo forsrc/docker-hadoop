ea0e5952c2bcdd0d2ec962e7d09a46d785dbce71
forsrc <forsrc@gmail.com>
2018-11-03 01:43:34 +0800

    spark

--- bigdata/tools/start-spark.sh
+++ bigdata/tools/start-spark.sh
@@ -7,5 +7,12 @@ echo `hostname` ... START
 echo $SPARK_HOME/sbin/start-all.sh
 $SPARK_HOME/sbin/start-all.sh
 
+echo $SPARK_HOME/sbin/start-history-server.sh
+mkdir /tmp/spark-events
+$SPARK_HOME/sbin/start-history-server.sh
+
+echo $SPARK_HOME/bin/spark-shell
+$SPARK_HOME/bin/spark-shell
+
 echo `hostname` ... END
 echo ================================================ END
