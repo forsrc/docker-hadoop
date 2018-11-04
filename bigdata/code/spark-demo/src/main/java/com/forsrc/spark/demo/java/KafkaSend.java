1036362077f3e544f33f614e586066a1de9ab563
forsrc <forsrc@gmail.com>
2018-11-04 23:41:19 +0800

    spark streaming

---
+++ bigdata/code/spark-demo/src/main/java/com/forsrc/spark/demo/java/KafkaSend.java
@@ -0,0 +1,75 @@
+package com.forsrc.spark.demo.java;
+
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.Producer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.apache.kafka.common.serialization.StringSerializer;
+
+import java.util.Arrays;
+import java.util.Properties;
+
+public class KafkaSend {
+
+    public static void main(String[] args) {
+
+        String topic = "topic-spark-streaming-wordcount";
+
+        Properties props = new Properties();
+        props.put("bootstrap.servers", "localhost:9092");
+        props.put("group.id", "group-spark-streaming-test");
+
+        //producer
+        //Set acknowledgements for producer requests.
+        props.put("acks", "all");
+        //If the request fails, the producer can automatically retry,
+        props.put("retries", 0);
+        //Specify buffer size in config
+        props.put("batch.size", 16384);
+        //Reduce the no of requests less than 0
+        props.put("linger.ms", 1);
+        //The buffer.memory controls the total amount of memory available to the producer for buffering.
+        props.put("buffer.memory", 33554432);
+        props.put("key.serializer", StringSerializer.class.getName());
+        props.put("value.serializer", StringSerializer.class.getName());
+
+        //consumer
+        props.put("enable.auto.commit", "true");
+        props.put("auto.commit.interval.ms", "1000");
+        props.put("session.timeout.ms", "30000");
+        props.put("key.deserializer", StringDeserializer.class.getName());
+        props.put("value.deserializer", StringDeserializer.class.getName());
+
+
+        KafkaConsumer<String, String> consumer = new KafkaConsumer<>(props);
+        consumer.subscribe(Arrays.asList(topic));
+        new Thread() {
+            @Override
+            public void run() {
+                while (true) {
+                    ConsumerRecords<String, String> records = consumer.poll(100);
+                    for (ConsumerRecord<String, String> record : records) {
+                        System.out.printf("records -> offset = %d, key = %s, value = %s", record.offset(), record.key(), record.value());
+                        System.out.println();
+                    }
+                }
+            }
+        }//.start()
+        ;
+
+        Producer<String, String> producer = new KafkaProducer<String, String>(props);
+
+        for (int i = 0; i < 10; i++) {
+            ProducerRecord producerRecord = new ProducerRecord<String, String>(topic, Integer.toString(i), "a b c " + Integer.toString(i));
+            producer.send(producerRecord);
+            System.out.println("send -> " + producerRecord);
+        }
+        System.out.println("Message sent successfully");
+        producer.close();
+
+
+    }
+}
---
+++ bigdata/code/spark-demo/src/main/java/com/forsrc/spark/demo/java/StreamingWordCount.java
@@ -0,0 +1,75 @@
+package com.forsrc.spark.demo.java;
+
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.apache.spark.SparkConf;
+import org.apache.spark.streaming.Durations;
+import org.apache.spark.streaming.api.java.JavaDStream;
+import org.apache.spark.streaming.api.java.JavaInputDStream;
+import org.apache.spark.streaming.api.java.JavaPairDStream;
+import org.apache.spark.streaming.api.java.JavaStreamingContext;
+import org.apache.spark.streaming.kafka010.ConsumerStrategies;
+import org.apache.spark.streaming.kafka010.KafkaUtils;
+import org.apache.spark.streaming.kafka010.LocationStrategies;
+import scala.Tuple2;
+
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+
+public class StreamingWordCount {
+
+    public static void main(String[] args) {
+
+        String zk = "localhost:2181";
+        String topic = "topic-spark-streaming-wordcount";
+
+        SparkConf sparkConf = new SparkConf()
+                .setAppName("forsrc-spark-streaming-wordcount")
+                .setMaster("local[4]");
+
+        JavaStreamingContext javaStreamingContext = new JavaStreamingContext(sparkConf, Durations.seconds(10));
+
+        Map<String, Object> kafkaParams = new HashMap<>();
+        kafkaParams.put("bootstrap.servers", "localhost:9092");
+        //kafkaParams.put("key.serializer", StringSerializer.class);
+        //kafkaParams.put("value.serializer", StringSerializer.class);
+        kafkaParams.put("key.deserializer", StringDeserializer.class);
+        kafkaParams.put("value.deserializer", StringDeserializer.class);
+        kafkaParams.put("group.id", "group-spark-streaming-test");
+        kafkaParams.put("auto.offset.reset", "latest");
+        kafkaParams.put("enable.auto.commit", false);
+
+        Collection<String> topics = Arrays.asList(topic);
+
+        JavaInputDStream<ConsumerRecord<String, String>> stream =
+                KafkaUtils.createDirectStream(
+                        javaStreamingContext,
+                        LocationStrategies.PreferConsistent(),
+                        ConsumerStrategies.<String, String>Subscribe(topics, kafkaParams)
+                );
+
+        JavaPairDStream<String, String> records = stream.mapToPair(record -> new Tuple2<>(record.key(), record.value()));
+
+        JavaDStream<String> words = records.flatMap(t -> Arrays.asList(t._2.split(" ")).iterator());
+
+        JavaPairDStream<String, Integer> pairs = words.mapToPair(s -> new Tuple2<>(s, 1));
+        JavaPairDStream<String, Integer> wordCounts = pairs.reduceByKey((i1, i2) -> i1 + i2);
+
+        wordCounts.print();
+        javaStreamingContext.start();              // Start the computation
+        try {
+            javaStreamingContext.awaitTermination();   // Wait for the computation to terminate
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } finally {
+
+            javaStreamingContext.close();
+        }
+
+    }
+
+
+}
