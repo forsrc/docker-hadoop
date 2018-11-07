b6ba87102fda514a01252a0a9ebe34849df8b139
forsrc <forsrc@gmail.com>
2018-11-07 22:28:12 +0800

    hadoop-3.1.1

--- bigdata/tools/start-hadoop.sh
+++ bigdata/tools/start-hadoop.sh
@@ -8,10 +8,18 @@ echo [*] $HADOOP_HOME/sbin/start-dfs.sh
 $HADOOP_HOME/sbin/start-dfs.sh
 echo [*] $HADOOP_HOME/sbin/start-yarn.sh
 $HADOOP_HOME/sbin/start-yarn.sh
-echo [*] hadoop-daemon.sh start datanode
-hadoop-daemon.sh start datanode
-echo [*] $HADOOP_HOME/sbin/mr-jobhistory-daemon.sh start historyserver
-$HADOOP_HOME/sbin/mr-jobhistory-daemon.sh start historyserver
+
+# hadoop-2.7.7
+#echo [*] hadoop-daemon.sh start datanode
+#hadoop-daemon.sh start datanode
+#echo [*] $HADOOP_HOME/sbin/mr-jobhistory-daemon.sh start historyserver
+#$HADOOP_HOME/sbin/mr-jobhistory-daemon.sh start historyserver
+
+# hadoop-3.1.1
+echo [*] hdfs --daemon start datanode
+hdfs --daemon start datanode
+echo [*] mapred --daemon start historyserver
+mapred --daemon start historyserver
 sleep 2
 
 echo [*] `hostname` ... END
